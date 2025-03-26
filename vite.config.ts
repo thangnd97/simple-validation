724c32d5bc9aae1aed753c66fe38912ccfc102ea
thangnd97 <thangnd.hbtt@gmail.com>
2025-03-26 17:05:43 +0700

    update build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import {defineConfig} from 'vite'
+import dts from 'vite-plugin-dts'
+import {resolve} from 'path'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [dts({include: ['lib']})],
+    build: {
+        copyPublicDir: false,
+        lib: {
+            entry: resolve(__dirname, 'lib/main.ts'),
+            formats: ['es']
+        },
+    }
 })
