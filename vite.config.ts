2704b66e7b23b2f32c60fa910fa94b3fdc95ecd7
thangnd97 <thangnd.hbtt@gmail.com>
2025-03-27 08:51:08 +0700

    update build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import {defineConfig} from 'vite'
 import dts from 'vite-plugin-dts'
-import {resolve} from 'path'
+import {extname, relative, resolve} from 'path'
+import {fileURLToPath} from "node:url";
+import {glob} from "glob";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [dts({include: ['lib']})],
@@ -10,5 +12,20 @@ export default defineConfig({
             entry: resolve(__dirname, 'lib/main.ts'),
             formats: ['es']
         },
+        rollupOptions: {
+            external: ['react', 'react/jsx-runtime', '@remix-run/react', '@shopify/remix-oxygen'],
+            input: Object.fromEntries(
+                glob.sync('lib/**/*.{ts,tsx}', {
+                    // ignore: ["lib/**/*.d.ts"],
+                }).map(file => [
+                    relative('lib', file.slice(0, file.length - extname(file).length)),
+                    fileURLToPath(new URL(file, import.meta.url))
+                ])
+            ),
+            output: {
+                assetFileNames: 'assets/[name][extname]',
+                entryFileNames: '[name].js'
+            }
+        }
     }
 })
